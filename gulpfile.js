4c75a88b1217bbae67652c22befaee3c2c0a26ec
Dmitrij <dmitrij.khramov@bk.ru>
2023-04-05 08:49:55 +0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-const {src, dest} = require('gulp');
+const {src, dest, watch, parallel, series} = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
 
 function styles() {
     return src('app/scss/style.scss')
         .pipe(concat('style.min.css'))
         .pipe(scss({outputStyle: 'compressed'}))
         .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
 }
 
 function scripts() {
@@ -16,7 +19,42 @@ function scripts() {
         .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(dest('app/min_js'))
+        .pipe(browserSync.stream())
+}
+
+function watching() {
+    watch(['app/scss/style.scss'], styles)
+    watch(['app/js/main.js'], scripts)
+    watch(['app/**/*.html']).on('change', browserSync.reload)
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+function cleanDist() {
+    return src('dist')
+        .pipe(clean())
+}
+
+function building() {
+    return src([
+        'app/css/base.css',
+        'app/css/style.min.css',
+        'app/min_js/main.min.js',
+        'app/**/*.html'
+    ], {base: 'app'})
+        .pipe(dest('dist'))
 }
 
 exports.styles = styles;
 exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.build = series(cleanDist, building)
+exports.default = parallel(styles, scripts, browsersync, watching);
