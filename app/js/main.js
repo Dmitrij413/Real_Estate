f5ed90bc78535210fe06437f72e811800741f0d0
Dmitrij <dmitrij.khramov@bk.ru>
2023-04-15 12:16:53 +0400

    Changes to be committed: 	modified:   app/css/style.min.css 	modified:   app/index.html 	modified:   app/js/main.js 	modified:   app/min_js/main.min.js 	modified:   app/scss/style.scss

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 const inputMinValue = document.getElementById('min'); // Ищем input с меньшим значнием
 const inputMaxValue = document.getElementById('max'); // Ищем input с большим значнием
 const priceRange = document.getElementById('price-range');
